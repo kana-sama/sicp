36c78c7b3f3a94910407b6672a9901e7f484eaec
kana <andrew4chrome@gmail.com>
2017-04-28 20:12:55 +0300

    add 1.11

---
+++ 1.11.scm
@@ -0,0 +1,16 @@
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1))
+                 (f (- n 2))
+                 (f (- n 3))))))
+
+(define (f* n)
+  (let loop ((n n)
+             (a 1)
+             (b 2)
+             (c 3))
+    (case n
+      ((1) a)
+      ((2) b)
+      ((3) c)
+      (else (loop (- n 1) b c (+ a b c))))))
