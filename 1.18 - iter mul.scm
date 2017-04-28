726923ff176bb2dc63a486b351e9df76749bf4c3
kana <andrew4chrome@gmail.com>
2017-04-28 22:47:10 +0300

    add 1.18

---
+++ 1.18 - iter mul.scm
@@ -0,0 +1,7 @@
+(load "utils")
+
+(define (mul a b)
+  (let loop ((extra 0) (result a) (factor b))
+    (cond ((zero? factor) extra)
+          ((even? factor) (loop extra (double result) (half factor)))
+          (else (loop (+ extra result) result (dec factor))))))
