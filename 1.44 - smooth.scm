30c5debce35d15730033f4167974ed430f2b0280
kana <andrew4chrome@gmail.com>
2017-04-30 02:35:21 +0300

    add 1.44

---
+++ 1.44 - smooth.scm
@@ -0,0 +1,10 @@
+(load "1.43 - repeated self-compose.scm")
+
+(define dx 0.0001)
+(define (smooth f)
+  (lambda (x)
+    (average (f (- x dx))
+             (f x)
+             (f (+ x dx)))))
+
+(define smooth5 (repeated smooth 5))
