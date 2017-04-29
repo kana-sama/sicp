0a3533428731206078b929c7eaa10ddfc487a47c
kana <andrew4chrome@gmail.com>
2017-04-29 15:16:56 +0300

    fix 1.35/1.36; remove algo/fixed-point

--- 1.35 - search golden ratio by fixed point.scm
+++ 1.35 - search golden ratio by fixed point.scm
@@ -1,4 +1,9 @@
-(load "algo/fixed-point")
+(define (fixed-point f first-guess)
+  (let loop ((guess first-guess))
+    (let ((next-guess (f guess)))
+      (if (close? next-guess guess 0.00001)
+        next-guess
+        (loop next-guess)))))
 
 (define phi
   (fixed-point (compose inc reciprocal) 1.0))
--- 1.36.scm
+++ 1.36.scm
@@ -1,13 +1,15 @@
-(load "algo/fixed-point")
+(define (fixed-point f first-guess)
+  (let loop ((guess first-guess))
+    (print guess)
+    (let ((next-guess (f guess)))
+      (if (close? next-guess guess 0.00001)
+        next-guess
+        (loop next-guess)))))
 
-(print (fixed-point
-         (lambda (x)
-           (/ (log 1000)
-              (log x)))
-         2)
-       (fixed-point
-         (lambda (x)
-           (average x
-                    (/ (log 1000)
-                       (log x))))
-         2))
+(print
+  "Simple: "
+  (fixed-point (lambda (x) (/ (log 1000) (log x))) 2))
+
+(print
+  "Average: "
+  (fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 2))
