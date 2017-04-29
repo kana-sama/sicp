965faeb4e23861c7d50f2b98101b63166d1d94dc
kana <andrew4chrome@gmail.com>
2017-04-30 02:18:45 +0300

    add 1.43

---
+++ 1.43 - repeated self-compose.scm
@@ -0,0 +1,14 @@
+(define (repeated f n)
+  (let loop ((result identity) (i n))
+    (if (zero? i)
+      result
+      (loop (compose f result) (dec i)))))
+
+(define (repeated* f n)
+  (let loop ((extra identity) (result f) (i n))
+    (cond ((zero? i) extra)
+          ((even? i) (loop extra (compose result result) (half i)))
+          (else (loop (compose extra result) result (dec i))))))
+
+(print ((repeated  square 3) 5)
+       ((repeated* square 3) 5))
